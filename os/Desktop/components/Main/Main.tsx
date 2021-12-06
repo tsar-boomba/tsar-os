7fae91ba24feb214b4cc813dab95547d54bfe9c1
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-12-06 15:09:36 -0500

    stated work on window and app api

--- os/Desktop/components/Main/Main.tsx
+++ os/Desktop/components/Main/Main.tsx
@@ -1,11 +1,18 @@
 import styles from './Main.module.scss';
 import Image from 'next/image';
 import defaultBackground from '@/public/images/default-desktop.jpg';
+import { useContext } from 'react';
+import { AppsContext } from '../../context/AppsContext';
+import Window from '../Window';
 
 const Main = () => {
+	const { opened } = useContext(AppsContext);
+
 	return (
 		<div className={styles.container}>
-			<h1 style={{ margin: 0 }}>Main content</h1>
+			{opened.map((App, index) => (
+				<Window App={App} key={index} />
+			))}
 			<div className={styles.background}>
 				<Image src={defaultBackground} layout='fill' alt='desktop background' />
 			</div>
