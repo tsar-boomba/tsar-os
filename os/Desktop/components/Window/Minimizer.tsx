7af38de2705c81a284aa80d52fc6e0c565e665c8
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-12-10 15:16:15 -0500

    moved minimize to own component

---
+++ os/Desktop/components/Window/Minimizer.tsx
@@ -0,0 +1,65 @@
+import { useContext, useEffect } from 'react';
+import { AppsContext } from '../../context/AppsContext';
+import { BaseProps } from './Window';
+
+const Minimizer: React.VFC<BaseProps & { name: string }> = ({ windowRef, data, setData, name }) => {
+	const { apps } = useContext(AppsContext);
+
+	useEffect(() => {
+		const isFirstRender = data.last.height === '';
+		if (!windowRef.current) throw new Error('No window element found on ref.');
+		const windowEl = windowRef.current;
+
+		const removeTransition = () => (windowEl.style.transition = '');
+		windowEl.addEventListener('transitionend', removeTransition);
+
+		if (data.minimized) {
+			windowEl.style.transition =
+				'top 0.2s ease, left 0.2s ease, width 0.2s ease, height 0.2s ease';
+			const thisAppIndex = apps.findIndex((app) => app.name === name);
+			const LOGO_WIDTH = 36;
+			const APP_ICON_WIDTH = 46;
+			const targetX = LOGO_WIDTH + thisAppIndex * APP_ICON_WIDTH;
+
+			// saving last position
+			const styles = getComputedStyle(windowEl);
+			setData({
+				...data,
+				last: {
+					top: styles.top,
+					left: styles.left,
+					width: styles.width,
+					height: styles.height,
+				},
+			});
+
+			// have to set these in inline or else transition don't work
+			windowEl.style.left = styles.left;
+			windowEl.style.top = styles.top;
+			windowEl.style.width = styles.width;
+			windowEl.style.height = styles.height;
+
+			windowEl.style.left = `${targetX}px`;
+			windowEl.style.top = '100%';
+			windowEl.style.width = '0px';
+			windowEl.style.height = '0px';
+		} else {
+			// if first render do nothing
+			if (!isFirstRender) {
+				windowEl.style.transition =
+					'top 0.2s ease, left 0.2s ease, width 0.2s ease, height 0.2s ease';
+				// returning to last position
+				windowEl.style.left = data.last.left;
+				windowEl.style.top = data.last.top;
+				windowEl.style.width = data.last.width;
+				windowEl.style.height = data.last.height;
+			}
+		}
+
+		return () => windowEl.removeEventListener('transitionend', removeTransition);
+	}, [data.minimized]);
+
+	return <></>;
+};
+
+export default Minimizer;
--- os/Desktop/components/Window/Window.tsx
+++ os/Desktop/components/Window/Window.tsx
@@ -1,9 +1,10 @@
 import { useContext, useEffect, useRef, useState } from 'react';
 import { AppsContext, OSApp } from '../../context/AppsContext';
+import Minimizer from './Minimizer';
 import Resizers from './Resizers';
 import styles from './Window.module.scss';
 
-interface BaseProps {
+export interface BaseProps {
 	closeWindow: () => void;
 	titleBarRef: React.RefObject<HTMLDivElement>;
 	windowRef: React.RefObject<HTMLDivElement>;
@@ -31,70 +32,23 @@ const Window: React.VFC<Props> = ({ App, TitleBar, name, icon, setData, data }) 
 	const titleBarRef = useRef<HTMLDivElement>(null);
 	const windowRef = useRef<HTMLDivElement>(null);
 	const [windowIndex, setWindowIndex] = useState(opened.findIndex((app) => app.name === name));
-
-	// updating this window's zIndex when opened changes
-	useEffect(() => setWindowIndex(opened.findIndex((app) => app.name === name)), [opened]);
-
 	const closeWindow = () => {
 		const newOpened = [...opened];
 		newOpened.splice(windowIndex, 1);
 		setOpened(newOpened);
 	};
 
-	// handling minimize state
-	useEffect(() => {
-		const isFirstRender = data.last.height === '';
-		if (!windowRef.current) throw new Error('No window element found on ref.');
-		const windowEl = windowRef.current;
+	// base props to be passed to components
+	const baseProps: BaseProps = {
+		closeWindow,
+		data,
+		setData,
+		titleBarRef,
+		windowRef,
+	};
 
-		const removeTransition = () => (windowEl.style.transition = '');
-		windowEl.addEventListener('transitionend', removeTransition);
-
-		if (data.minimized) {
-			windowEl.style.transition =
-				'top 0.2s ease, left 0.2s ease, width 0.2s ease, height 0.2s ease';
-			const thisAppIndex = apps.findIndex((app) => app.name === name);
-			const LOGO_WIDTH = 36;
-			const APP_ICON_WIDTH = 46;
-			const targetX = LOGO_WIDTH + thisAppIndex * APP_ICON_WIDTH;
-
-			// saving last position
-			const styles = getComputedStyle(windowEl);
-			setData({
-				...data,
-				last: {
-					top: styles.top,
-					left: styles.left,
-					width: styles.width,
-					height: styles.height,
-				},
-			});
-
-			// have to set these in inline or else transition don't work
-			windowEl.style.left = styles.left;
-			windowEl.style.top = styles.top;
-			windowEl.style.width = styles.width;
-			windowEl.style.height = styles.height;
-
-			windowEl.style.left = `${targetX}px`;
-			windowEl.style.top = '100%';
-			windowEl.style.width = '0px';
-			windowEl.style.height = '0px';
-		} else {
-			// if first render do nothing
-			if (!isFirstRender) {
-				windowEl.style.transition =
-					'top 0.2s ease, left 0.2s ease, width 0.2s ease, height 0.2s ease';
-				// returning to last position
-				windowEl.style.left = data.last.left;
-				windowEl.style.top = data.last.top;
-				windowEl.style.width = data.last.width;
-				windowEl.style.height = data.last.height;
-			}
-		}
-
-		return () => windowEl.removeEventListener('transitionend', removeTransition);
-	}, [data.minimized]);
+	// updating this window's zIndex when opened changes
+	useEffect(() => setWindowIndex(opened.findIndex((app) => app.name === name)), [opened]);
 
 	const handleFocus = () => {
 		const tempOpened = [...opened];
@@ -110,24 +64,14 @@ const Window: React.VFC<Props> = ({ App, TitleBar, name, icon, setData, data }) 
 			className={styles.container}
 			ref={windowRef}
 		>
-			<TitleBar
-				content={name}
-				closeWindow={closeWindow}
-				titleBarRef={titleBarRef}
-				windowRef={windowRef}
-				data={data}
-				setData={setData}
-			/>
-			<App
-				closeWindow={closeWindow}
-				windowRef={windowRef}
-				titleBarRef={titleBarRef}
-				data={data}
-				setData={setData}
-			/>
+			<TitleBar content={name} {...baseProps} />
+			<App {...baseProps} />
 
 			{/* Window resizers */}
 			<Resizers windowRef={windowRef} />
+
+			{/* Handles minimizing window */}
+			<Minimizer {...baseProps} name={name} />
 		</div>
 	);
 };
